d90cce40466c05411b0443a9842824f947c782dd
JulkaUlka <y.glushko@outlook.com>
2024-10-30 18:35:24 +0200

    feat: improve Stripe implementation

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
-app.post('/webhook-checkout', express.raw({ type: 'application/json' }),bookingController.webhookCheckout());
+app.post('/webhook-checkout', express.raw({ type: 'application/json' }),bookingController.webhookCheckout);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
